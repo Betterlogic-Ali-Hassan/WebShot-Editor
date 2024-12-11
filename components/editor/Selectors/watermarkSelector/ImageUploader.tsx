1d1bfd4b59a52d429bc553fda205a516cfd7a98b
HassanThePassionate <maqsoodia777@gmail.com>
2024-12-11 16:20:45 +0500

    Update

--- components/editor/Selectors/watermarkSelector/ImageUploader.tsx
+++ components/editor/Selectors/watermarkSelector/ImageUploader.tsx
@@ -1,15 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { useImageUpload } from "@/hooks/use-image-upload";
 import { Upload } from "lucide-react";
 import React from "react";
 
 const ImageUploader = () => {
+  const { fileInputRef, handleFileChange, handleThumbnailClick } =
+    useImageUpload();
   return (
     <div className='space-y-2'>
-      <Button variant='outline' className='w-full h-24 border-dashed'>
+      <Button
+        variant='outline'
+        className='w-full h-24 border-dashed'
+        onClick={handleThumbnailClick}
+      >
         <div className='flex flex-col items-center gap-2'>
           <Upload className='w-5 h-5' />
           <span>Upload Image</span>
         </div>
+        <input
+          type='file'
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          className='hidden'
+          accept='image/*'
+          aria-label='Upload image file'
+        />
       </Button>
     </div>
   );
