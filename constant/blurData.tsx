027c1407423373d4b071a18240b65daacc78f6a3
HassanThePassionate <maqsoodia777@gmail.com>
2024-12-12 17:29:48 +0500

    Update

--- constant/blurData.tsx
+++
@@ -1,8 +0,0 @@
-import { Drop } from "@/components/svgs";
-
-export const blurData = [
-  { id: 1, name: "Blur Area 1", icon: <Drop height={20} width={20} /> },
-  { id: 2, name: "Blur Area 2", icon: <Drop height={20} width={20} /> },
-  { id: 3, name: "Blur Area 3", icon: <Drop height={20} width={20} /> },
-  { id: 4, name: "Blur Area 4", icon: <Drop height={20} width={20} /> },
-];
