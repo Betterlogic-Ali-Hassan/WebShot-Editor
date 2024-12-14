5ac373c800ff00c6a8dcacd2c89e2f09ec7e2b01
HassanThePassionate <maqsoodia777@gmail.com>
2024-12-14 22:51:09 +0500

    Update

--- components/DummyImage.tsx
+++ components/DummyImage.tsx
@@ -5,11 +5,12 @@ const DummyImage = () => {
   return (
     <div className='img pt-[200px]  overflow-auto'>
       <Image
-        src='/dummy.jpg'
+        src='/dummy.png'
         alt='img'
         height={2000}
         width={2000}
-        className='object-cover w-screen'
+        className='object-contain '
+        priority
       />
     </div>
   );
