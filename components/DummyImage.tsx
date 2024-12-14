8b85ffd2a39b569304820831434f63896d4b0114
HassanThePassionate <maqsoodia777@gmail.com>
2024-12-14 22:54:11 +0500

    Update

--- components/DummyImage.tsx
+++ components/DummyImage.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const DummyImage = () => {
   return (
-    <div className='img pt-[200px]  overflow-auto'>
+    <div className='min-h-screen pt-[190px]  overflow-auto'>
       <Image
         src='/dummy.png'
         alt='img'
         height={2000}
         width={2000}
-        className='object-contain '
+        className='object-cover '
         priority
       />
     </div>
