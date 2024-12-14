a2a2f69306417e1ae5c725af0e5a868a7d1a762c
HassanThePassionate <maqsoodia777@gmail.com>
2024-12-14 23:26:32 +0500

    Update

--- components/DummyImage.tsx
+++ components/DummyImage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const DummyImage = () => {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen pt-[105px]'>
       <Image
         src='/dummy.png'
         alt='img'
