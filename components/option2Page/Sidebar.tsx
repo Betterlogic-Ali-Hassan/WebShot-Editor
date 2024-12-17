cfa2d04d33a6e27c0851e3a4f6ab88737dd4b690
HassanThePassionate <maqsoodia777@gmail.com>
2024-12-17 19:23:56 +0500

    Update

--- components/option2Page/Sidebar.tsx
+++ components/option2Page/Sidebar.tsx
@@ -6,7 +6,7 @@ import { InstaLogo } from "../svgs";
 
 export function Sidebar() {
   return (
-    <aside className='min-w-[335px] h-screen px-3 pt-2 pb-3 flex flex-col  border-r sticky top-0 bg-white'>
+    <aside className='w-[335px] max-[1400px]:w-[244px] h-screen px-3 pt-2 pb-3 flex flex-col  border-r sticky top-0 bg-white'>
       <div className='pt-8 px-3 pb-4 mb-4 w-full'>
         <InstaLogo />
       </div>
