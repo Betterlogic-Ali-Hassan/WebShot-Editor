919edb9bb762c84d4cca048274927cb4d337882e
HassanThePassionate <maqsoodia777@gmail.com>
2024-12-23 01:17:21 +0500

    Update

--- components/editor/Selectors/exportSelector/ExportContent.tsx
+++ components/editor/Selectors/exportSelector/ExportContent.tsx
@@ -1,16 +1,20 @@
 import { exportData } from "@/constant/ExportData";
 import { useToast } from "@/hooks/use-toast";
 import { cn } from "@/lib/utils";
-import React from "react";
 
 const ExportContent = () => {
   const { toast } = useToast();
+
   return (
     <>
       <ul className='flex  flex-col gap-0.5 min-w-[180px]'>
         {exportData.map((item, index) => (
           <li
-            onClick={() => toast({ description: item.toast })}
+            onClick={() => {
+              toast({
+                description: item.toast,
+              });
+            }}
             key={index}
             className={cn(
               "flex items-center gap-3 py-2 px-3  hover:bg-light cursor-pointer text-sm",
