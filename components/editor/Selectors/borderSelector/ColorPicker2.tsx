d14b8354e5b1c67762e768120b2bdfcb2c747e44
HassanThePassionate <maqsoodia777@gmail.com>
2024-12-23 17:40:54 +0500

    Update

--- components/editor/Selectors/borderSelector/ColorPicker2.tsx
+++ components/editor/Selectors/borderSelector/ColorPicker2.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Popover,
@@ -10,9 +9,7 @@ import {
 } from "@/components/ui/popover";
 import { ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { ChromePicker } from "react-color";
-import { Theme } from "@/components/svgs";
-import { FaCheck } from "react-icons/fa6";
+import { SketchPicker, ColorResult } from "react-color";
 
 export default function ColorPicker({
   select,
@@ -23,88 +20,44 @@ export default function ColorPicker({
 }) {
   const [selectedColor, setSelectedColor] = useState("#FF0000");
   const [recentColors, setRecentColors] = useState<string[]>([]);
-  const [showPicker, setShowPicker] = useState(false);
   const [popoverOpen, setPopoverOpen] = useState(false);
-  const handlePopOver = () => {
-    setPopoverOpen(false);
-    setShowPicker(false);
-  };
 
-  const colors = [
-    // Row 1
-    "#FF0000",
-    "#FF4D00",
-    "#FFD300",
-    "#00FF00",
-    "#00FFFF",
-    "#0000FF",
-    "#9900FF",
-    "#FF00FF",
-    "#FFFFFF",
-    // Row 2
-    "#FFB3B3",
-    "#FFD9B3",
-    "#FFF6B3",
-    "#B3FFB3",
-    "#B3FFFF",
-    "#B3B3FF",
-    "#F0B3FF",
-    "#FFB3FF",
-    "#F2F2F2",
-    // Row 3
-    "#FF6666",
-    "#FFB366",
-    "#FFED66",
-    "#66FF66",
-    "#66FFFF",
-    "#6666FF",
-    "#E066FF",
-    "#FF66FF",
-    "#E6E6E6",
-    // Row 4
-    "#FF0000",
-    "#FF8C00",
-    "#FFE500",
-    "#00FF00",
-    "#00FFFF",
-    "#0000FF",
-    "#CC00FF",
-    "#FF00FF",
-    "#CCCCCC",
-    // Row 5
-    "#CC0000",
-    "#CC7000",
-    "#CCB800",
-    "#00CC00",
-    "#00CCCC",
-    "#0000CC",
-    "#A300CC",
-    "#CC00CC",
-    "#999999",
-  ];
+  useEffect(() => {
+    const storedRecentColors = localStorage.getItem("recentColors");
+    if (storedRecentColors) {
+      setRecentColors(JSON.parse(storedRecentColors).slice(0, 12));
+    }
+  }, []);
 
   const handleColorSelect = (color: string) => {
     setSelectedColor(color);
-    if (!recentColors.includes(color)) {
-      setRecentColors((prev) => [color, ...prev.slice(0, 3)]);
-    }
+    setRecentColors((prev) => {
+      const updatedColors = [color, ...prev.filter((c) => c !== color)].slice(
+        0,
+        12
+      );
+      localStorage.setItem("recentColors", JSON.stringify(updatedColors));
+      return updatedColors;
+    });
   };
-  const handlePickerChangeComplete = (color: any) => {
+
+  const handlePickerChangeComplete = (color: ColorResult) => {
     handleColorSelect(color.hex);
   };
+
   return (
     <div className={cn("mt-5", select && "mt-0")}>
       <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
         <PopoverTrigger asChild>
           {select ? (
-            <Button variant='outline' className=' border-0 justify-start p-2'>
+            <Button variant='outline' className='border-0 justify-start p-2'>
               <div
                 className='h-5 w-5 rounded border'
                 style={{ backgroundColor: selectedColor }}
               />
               {!icon ? (
                 <span>
-                  <ChevronDown className='h-4 w-4 ' />
+                  <ChevronDown className='h-4 w-4' />
                 </span>
               ) : (
                 <p className='whitespace-nowrap cursor-pointer'>
@@ -125,79 +78,15 @@ export default function ColorPicker({
           )}
         </PopoverTrigger>
         <PopoverContent
-          className='min-w-[240px] p-3 bg-white mt-0 max-h-[350px] overflow-y-auto scrollbar '
+          className='p-0 bg-white mt-0 overflow-y-auto scrollbar'
           side='bottom'
         >
-          <div className='p-2'>
-            <div className='flex items-center justify-between mb-3'>
-              <Button variant='outline' className=' justify-start'>
-                <div className='flex items-center gap-2'>
-                  <div
-                    className='h-5 w-5 rounded border'
-                    style={{ backgroundColor: selectedColor }}
-                  />
-                  <span className='uppercase'>{selectedColor}</span>
-                </div>
-              </Button>
-              {!showPicker ? (
-                <Button
-                  variant='outline'
-                  size='icon'
-                  className='h-8 w-8'
-                  onClick={() => setShowPicker(true)}
-                >
-                  <Theme />
-                </Button>
-              ) : (
-                <Button
-                  variant='outline'
-                  size='icon'
-                  className='h-8 w-8'
-                  onClick={handlePopOver}
-                >
-                  <FaCheck size={20} />
-                </Button>
-              )}
-            </div>
-
-            {showPicker ? (
-              <ChromePicker
-                color={selectedColor}
-                onChangeComplete={handlePickerChangeComplete}
-                disableAlpha={true}
-              />
-            ) : (
-              <>
-                <div className='grid grid-cols-9 gap-1'>
-                  {colors.map((color, index) => (
-                    <button
-                      key={index}
-                      className='w-5 h-5 rounded border border-gray-200 focus:outline-none focus:ring-2 focus:ring-primary'
-                      style={{ backgroundColor: color }}
-                      onClick={() => handleColorSelect(color)}
-                    />
-                  ))}
-                </div>
-                {recentColors.length > 0 && (
-                  <div className='mt-3'>
-                    <div className='text-xs text-gray-500 mb-1'>
-                      Recent Colors
-                    </div>
-                    <div className='flex gap-1'>
-                      {recentColors.map((color, index) => (
-                        <button
-                          key={index}
-                          className='w-5 h-5 rounded border border-gray-200 focus:outline-none focus:ring-2 focus:ring-primary'
-                          style={{ backgroundColor: color }}
-                          onClick={() => handleColorSelect(color)}
-                        />
-                      ))}
-                    </div>
-                  </div>
-                )}
-              </>
-            )}
-          </div>
+          <SketchPicker
+            color={selectedColor}
+            onChangeComplete={handlePickerChangeComplete}
+            className='min-w-[220px]'
+            presetColors={recentColors}
+          />
         </PopoverContent>
       </Popover>
     </div>
