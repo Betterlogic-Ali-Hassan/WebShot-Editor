a4840614c71541f33f441a75652acc0a30907e9b
HassanThePassionate <maqsoodia777@gmail.com>
2024-12-24 23:08:36 +0500

    Update

--- components/RevisionCard.tsx
+++ components/RevisionCard.tsx
@@ -5,19 +5,19 @@ import { Trash } from "lucide-react";
 
 const RevisionCard = () => {
   return (
-    <div className='bg-white flex items-center justify-center gap-2 px-2 focus:outline-none border-border fixed bottom-3 left-1/2 -translate-x-1/2 min-w-[180px]  border bg-popover py-2 shadow-md z-40 rounded-full'>
+    <div className='bg-white flex items-center justify-center gap-2 px-2 focus:outline-none border-border fixed bottom-4 left-1/2 -translate-x-1/2 min-w-[180px]  border bg-popover py-2 shadow-md z-40 rounded-full'>
       {revisionData.map((item, id) => (
         <Tooltip
           trigger={item.icon}
           key={id}
           content={item.name}
-          contentClass='bg-black text-white'
+          contentClass='bg-black text-white rounded-full'
         />
       ))}
       <Tooltip
         trigger={<Trash size={20} />}
         content='Delete'
-        contentClass='bg-black text-white'
+        contentClass='bg-black text-white rounded-full'
       />
     </div>
   );
