72904e09b64ca3dc5040a6f728684afbdbd500e8
HassanThePassionate <maqsoodia777@gmail.com>
2025-01-01 18:08:42 +0500

    Update

--- components/editor/Selectors/borderSelector/BorderSelector.tsx
+++ components/editor/Selectors/borderSelector/BorderSelector.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ToolCard from "../../ToolCard";
 import { Border } from "@/components/svgs";
 import ToolDropdown from "@/components/ToolDropdown";
-import BrowserContent from "./BorderContent";
+import { BrowserContent } from "./BorderContent";
 
 const BorderSelector = () => {
   return (
