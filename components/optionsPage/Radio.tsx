e7b3f02e919a5b4bb1ad01b0a01aae5b667b2030
HassanThePassionate <maqsoodia777@gmail.com>
2025-01-02 16:55:04 +0500

    Update

--- components/optionsPage/Radio.tsx
+++ components/optionsPage/Radio.tsx
@@ -34,28 +34,5 @@ export function Radio({ title, options, defaultValue }: Props) {
         ))}
       </RadioGroup>
     </div>
-    // <div >
-    //   <Label className='font-medium py-2'>{title}</Label>
-    //   <RadioGroup
-    //     defaultValue={defaultValue}
-    //     onValueChange={(value) => onChange?.(value)}
-    //     className='flex items-center gap-x-6 gap-y-3 flex-wrap'
-    //   >
-    //     {options.map((option) => (
-    //       <div
-    //         key={option.value}
-    //         className='flex items-center gap-[10px] text-sm cursor-pointer hover:bg-[#f2f2f2] dark:hover:bg-[#535353] px-4 text-[15px] border w-fit h-[44px] rounded-lg dark:border-[#383838] border-[#dadada]'
-    //         onClick={() => onChange?.(option.value)}
-    //       >
-    //         <RadioGroupItem
-    //           value={option.value}
-    //           id={`${title}-${option.value}`}
-    //           className='w-[18px] h-[18px]'
-    //         />
-    //         <Label htmlFor={`${title}-${option.value}`}>{option.label}</Label>
-    //       </div>
-    //     ))}
-    //   </RadioGroup>
-    // </div>
   );
 }
